d8db0493a91a4836c7b3d130fba6ea1dbbcdad67
i994114 <i994114@hotmail.com>
2026-07-24 15:33:10 +0900

    デバッグ用にヘッダーにログインユーザ名表示

--- header.php
+++ header.php
@@ -1,22 +1,27 @@
+<?php 
+  $user = getUserInfoOne($_SESSION['user_id']);
+?>
+
 <header>
-      <div class="site-width">
-        <h1><a href="index.php"><?php echo APL_NAME.APL_SUBNAME; ?></a></h1>
-        <nav id="top-nav">
-          <ul>
-            <?php
-              if (empty($_SESSION['user_id'])) {
-            ?>
-                <li><a href="signup.php" class="btn btn-primary">ユーザ登録</a></li>
-                <li><a href="login.php">ログイン</a></li>
-            <?php
-              } else {
-            ?>
-                <li><a href="mypage.php">マイページ</a></li>
-                <li><a href="logout.php">ログアウト</a></li>
-            <?php
-              }
-            ?>
-          </ul>
-        </nav>
-      </div>
-    </header>
+  <div class="site-width">
+    <h1><a href="index.php"><?php echo APL_NAME.APL_SUBNAME; ?></a></h1>
+    <nav id="top-nav">
+      <ul>
+        <?php
+          if (empty($_SESSION['user_id'])) {
+        ?>
+            <li><a href="signup.php" class="btn btn-primary">ユーザ登録</a></li>
+            <li><a href="login.php">ログイン</a></li>
+        <?php
+          } else {
+        ?>
+            <li><a href="mypage.php">マイページ</a></li>
+            <li><a href="logout.php">ログアウト</a></li>
+            <p><?php echo $user['name']?></p>
+        <?php
+          }
+        ?>
+      </ul>
+    </nav>
+  </div>
+</header>
