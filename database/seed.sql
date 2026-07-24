79725842aa2b8bdd94284e189cfc2a7729d66fbe
i994114 <i994114@hotmail.com>
2026-07-24 15:12:53 +0900

    seeder変更

--- database/seed.sql
+++ database/seed.sql
@@ -5,14 +5,18 @@
 INSERT INTO users (
     id, name, email, password, gender, age, pic, delete_flg, login_time, create_date
 ) VALUES
-(1, '山田 太郎', 'taro@example.com', 'password', 0, 30, 'user1.jpg', 0, NOW(), NOW()),
-(2, '佐藤 花子', 'hanako@example.com', 'password', 1, 28, 'user2.jpg', 0, NOW(), NOW()),
-(3, '鈴木 一郎', 'ichiro@example.com', 'password', 0, 35, 'user3.jpg', 0, NOW(), NOW()),
-(4, '高橋 美咲', 'misaki@example.com', 'password', 1, 26, 'user4.jpg', 0, NOW(), NOW()),
-(5, '伊藤 健太', 'kenta@example.com', 'password', 0, 41, 'user5.jpg', 0, NOW(), NOW()),
-(6, '渡辺 彩', 'aya@example.com', 'password', 1, 33, 'user6.jpg', 0, NOW(), NOW()),
-(7, '中村 翔', 'sho@example.com', 'password', 0, 29, 'user7.jpg', 0, NOW(), NOW()),
-(8, 'bba', 'bba@example.com', 'password', 0, 29, 'bba.jpg', 0, NOW(), NOW());
+(1, 'Aliceeeeeeeeeeeeeeeeeeeeee', 'dorakue838861@gmail.com', 'password', 1, 30, 'user1.jpg', 0, NOW(), NOW()),
+(2, 'ウェブカツ試験太郎試験太郎試験太郎', 'dorakue83886.1@gmail.com', 'password', 0, 28, 'user2.jpg', 0, NOW(), NOW()),
+(3, 'Emma', 'dorakue838861+a@gmail.com', 'password', 1, 35, 'user3.jpg', 0, NOW(), NOW()),
+(4, 'ひなた', 'dorakue838861+b@gmail.com', 'password', 1, 26, 'user4.jpg', 0, NOW(), NOW()),
+(5, 'Lucas', 'dorakue838861+c@gmail.com', 'password', 0, 41, 'user5.jpg', 0, NOW(), NOW()),
+(6, 'さくら🌸', 'dorakue838861+d@gmail.com', 'password', 1, 33, 'user6.jpg', 0, NOW(), NOW()),
+(7, 'Oliver', 'dorakue838861+e@gmail.com', 'password', 0, 29, 'user7.jpg', 0, NOW(), NOW()),
+(8, 'たける', 'dorakue838861+f@gmail.com', 'password', 0, 38, 'user8.jpg', 0, NOW(), NOW()),
+(9, 'Sophia', 'dorakue838861+g@gmail.com', 'password', 1, 24, 'user9.jpg', 0, NOW(), NOW()),
+(10, 'にこ😊', 'dorakue838861+h@gmail.com', 'password', 1, 31, 'user10.jpg', 0, NOW(), NOW()),
+(11, 'Liam', 'dorakue838861+i@gmail.com', 'password', 0, 36, 'user11.jpg', 0, NOW(), NOW()),
+(12, '試験太郎', 'dorakue838861+j@gmail.com', 'password', 0, 27, 'user12.jpg', 0, NOW(), NOW());
 -- ===================================
 -- categories
 -- ===================================
