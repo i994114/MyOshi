7ca90de00fdf7cd459ed23643b6b173716384b53
i994114 <i994114@hotmail.com>
2026-08-16 08:33:00 +0900

    .sqlをまとめて実行するsqlを新設

---
+++ database/setup.sql
@@ -0,0 +1,6 @@
+SOURCE database/drop_tables.sql;
+SOURCE database/create_tables.sql;
+SOURCE database/categories.sql;
+SOURCE database/prefectures.sql;
+SOURCE database/targets.sql;
+SOURCE database/seed.sql;
