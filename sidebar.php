93f344b805271d9cf983c89934663cff5d0acb6e
i994114 <i994114@hotmail.com>
2026-08-18 09:36:27 +0900

    withdraw.phpを削除し、サイドバーから「退会する」を削除

--- sidebar.php
+++ sidebar.php
@@ -2,5 +2,4 @@
         <a href="registEvent.php">イベント情報をアップする</a>
         <a href="profEdit.php">プロフィール編集</a>
         <a href="passEdit.php">パスワード変更</a>
-        <a href="withdraw.php">退会</a>
 </section>
