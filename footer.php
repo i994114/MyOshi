22022eba6717ef52c54ab1bbf55a802887d8d2ea
i994114 <i994114@hotmail.com>
2026-08-20 10:36:56 +0900

    リンクの飛び先をログイン有無で切り替えるように修正

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 <footer id="footer">
-      Copyright <a href="index.php"><?php echo APL_NAME;  ?></a>. All Rights Reserved.
-    </footer>
+  Copyright
+  <a href="<?php echo !empty($_SESSION['user_id']) ? 'index.php' : 'top.php'; ?>">
+    <?php echo APL_NAME; ?>
+  </a>.
+  All Rights Reserved.
+</footer>
 
-    <script src="js/vendor/jquery-2.2.2.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+<script src="js/vendor/jquery-2.2.2.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
 </html>
