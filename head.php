d2b9582233281e54e207bf778f670e83531942e2
i994114 <i994114@hotmail.com>
2026-08-20 10:28:23 +0900

    レスポンシブのベースを実装

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $siteTitle; ?> | <?php echo APL_NAME.APL_SUBNAME; ?></title>
     <link rel="stylesheet" href="style.css?v=<?php echo filemtime('style.css'); ?>">
 <!--
