f205cf2d22dc8238ae5cce1d6bc273715ed5bbb0
i994114 <i994114@hotmail.com>
2026-08-21 15:23:48 +0900

    サイドメニュー名変更

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <section id="sidebar">
-        <a href="registEvent.php">イベント情報をアップする</a>
-        <a href="profEdit.php">プロフィール編集</a>
-        <a href="passEdit.php">パスワード変更</a>
+        <a href="registEvent.php">イベント登録する</a>
+        <a href="profEdit.php">プロフィール編集する</a>
+        <a href="passEdit.php">パスワード変更する</a>
 </section>
