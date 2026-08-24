d3effe734768dc61beb976ff7221245adf6c7340
i994114 <i994114@hotmail.com>
2026-08-24 10:45:07 +0900

    head.php タブ名表示からサブタイトル削除

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $siteTitle; ?> | <?php echo APL_NAME.APL_SUBNAME; ?></title>
+    <title><?php echo $siteTitle; ?> | <?php echo APL_NAME; ?></title>
     <link rel="stylesheet" href="style.css?v=<?php echo filemtime('style.css'); ?>">
 <!--
       <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
